4d567401f5e60363c7da06cf4a380e36dfcc83d0
Masaki Ishizaka <masaki.ishizaka@giftee.co>
2025-11-21 21:33:42 +0900

    feat: font を指定

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Noto Sans JP', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
